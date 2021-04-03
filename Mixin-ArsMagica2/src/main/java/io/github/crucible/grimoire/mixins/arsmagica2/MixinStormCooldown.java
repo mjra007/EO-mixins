1ffc5e811a305a4cdbcf65777763bee950b394f6
Micael Araujo <micael12jose@gmail.com>
2021-04-03 10:16:54 +0100

    Global cooldowns for weather and time spells.

---
+++ Mixin-ArsMagica2/src/main/java/io/github/crucible/grimoire/mixins/arsmagica2/MixinStormCooldown.java
@@ -0,0 +1,25 @@
+package io.github.crucible.grimoire.mixins.arsmagica2;
+
+import am2.spell.components.Storm;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = Storm.class, remap = false)
+public abstract class MixinStormCooldown {
+
+    @Inject(method = "applyEffectBlock", at = @At("HEAD"), cancellable = true)
+    public void applyEffectBlock(ItemStack stack, World world, int blockx, int blocky, int blockz, int blockFace, double impactX, double impactY, double impactZ, EntityLivingBase caster,  CallbackInfoReturnable<Boolean> ci) {
+      Util.checkIfSpellIsOnCooldown(caster, ci, false);
+    }
+
+    @Inject(method = "applyEffectEntity", at = @At("HEAD"), cancellable = true)
+    public void applyEffectEntity(ItemStack stack, World world, EntityLivingBase caster, Entity target,  CallbackInfoReturnable<Boolean> ci) {
+      Util.checkIfSpellIsOnCooldown(caster, ci, false);
+    }
+}
